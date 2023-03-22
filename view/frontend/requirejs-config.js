fc15a927d2e074d4963a322f00b9dab998e0cbad
msiewierska <marta.siewierska-stanczak@creativestyle.pl>
2023-03-22 12:29:13 +0100

    fix; [RK-708] Do not scroll page to the top when modal is opened

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Ui/js/modal/modal': {
+                'MageSuite_WidgetNotificationModal/js/modal-ext': true,
+            }
+        }
+    }
+}
---
+++ view/frontend/web/js/modal-ext.js
@@ -0,0 +1,39 @@
+/**
+ * Origin: Native M2 Modal Widget
+ * Modification type: extend
+ * Reasons:
+ * - add modal-no-scroll class to body when modal is opened and calculate scroll position
+ * - add no-scroll-child class to page-wrapper element
+ * - do not add additional above logic if body already has inline top style property
+ */
+ define(['jquery'], function ($) {
+    'use strict';
+
+    return function (mageModal) {
+        $.widget('mage.modal', mageModal, {
+            _createOverlay: function () {
+                if (this.options.modalClass.includes('notification-modal') && !$('.minicart-offcanvas-open').length) {
+                    const $currentTopOffset = window.scrollY;
+
+                    $('body')
+                        .addClass('modal-no-scroll')
+                        .css({ top: -$currentTopOffset });    
+                }
+
+                this._super();
+            },
+            _destroyOverlay: function () {
+                this._super();
+
+                if (this.options.modalClass.includes('notification-modal') && !$('.minicart-offcanvas-open').length) {
+                    const $currentTopOffset = $('body').css('top');
+
+                    $('body').removeClass('modal-no-scroll').css({ top: '' });    
+                    window.scrollTo(0, parseInt($currentTopOffset || '0', 10) * -1);
+                }
+            },
+        });
+
+        return $.mage.modal;
+    };
+});
